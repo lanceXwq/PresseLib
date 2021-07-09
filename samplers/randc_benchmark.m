e045217a3e732494977e969537dbb5310bc1a06c
Lance (Weiqing) Xu <lance.xwq@gmail.com>
2021-07-08 17:34:47 -0700

    use functions instead scripts

--- samplers/randc_benchmark.m
+++ samplers/randc_benchmark.m
@@ -6,33 +6,37 @@ function randc_benchmark(height, width)
 
     p = rand(height, width);
     seed = randi(1e6);
-    rng(seed)
 
     % the naive way
-    tic
-    s = cumsum(p, 2);
-    N = size(p, 1);
-    r = rand(N, 1) .* s(:, end);
-    for idx = N:-1:1
-        c1(idx, 1) = find(r(idx) < s(idx, :), 1);
-    end
-    toc
-    
+    rng(seed); tic; c1 = randc_old_1(p); toc
+
     % a way to avoid for loops
-    rng(seed)
-    tic
-        S = cumsum(p, 2);
-        [~, c2] = max(rand(size(p, 1), 1) .* S(:, end) < S, [], 2);
-    toc
-    
+    rng(seed); tic; c2 = randc_old_2(p); toc
+
     % (currently best) use the c api
-    rng(seed)
-    tic
-        c3 = randc(p);
-    toc
+    rng(seed); tic; c3 = randc(p); toc
 
     c2 = abs(c2 - c1);
     c3 = abs(c3 - c1);
+
     if any(c2 + c3)
         disp('inconsistent results')
     end
+
+end
+
+function c = randc_old_1(p)
+    s = cumsum(p, 2);
+    N = size(p, 1);
+    r = rand(N, 1) .* s(:, end);
+
+    for idx = N:-1:1
+        c(idx, 1) = find(r(idx) < s(idx, :), 1);
+    end
+
+end
+
+function c = randc_old_2(p)
+    S = cumsum(p, 2);
+    [~, c] = max(rand(size(p, 1), 1) .* S(:, end) < S, [], 2);
+end
